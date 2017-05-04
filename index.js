9785a584a0206a45b9a6b83b70a86ab73010b819
Koen903 <koensleurink@hotmail.com>
2017-05-04 19:48:25 +0200

    Fixed Reachable

--- index.js
+++ index.js
@@ -19,38 +19,39 @@ bot.on('message', message => {
 
   if (command === "ping") {
     message.channel.sendMessage('Pong!');
-    let isAan = ["BungeeCord","Lobby 1","Skyblock","Skywars","Factions","Eggwars","Adventure Escape","Arcade","Verstoppertje","Minetopia"];
+    let Namen = ["BungeeCord", "Lobby 1", "Skyblock", "Skywars", "Factions", "Eggwars", "Adventure Escape", "Arcade", "Verstoppertje", "Minetopia"];
+    let isAan = [false, false, false, false, false, false, false, false, false, false];
     isReachable('play.martycraft.net:25566').then(reachable => {
       isAan[0] = true;
       //=> true
     });
-	isReachable('play.martycraft.net:25567').then(reachable => {
+    isReachable('play.martycraft.net:25567').then(reachable => {
       isAan[1] = true;
       //=> true
     });
-	isReachable('play.martycraft.net:25568').then(reachable => {
+    isReachable('play.martycraft.net:25568').then(reachable => {
       isAan[2] = true;
       //=> true
     });
-	isReachable('play.martycraft.net:25569').then(reachable => {
+    isReachable('play.martycraft.net:25569').then(reachable => {
       isAan[3] = true;
       //=> true
     });
-	isReachable('play.martycraft.net:25570').then(reachable => {
+    isReachable('play.martycraft.net:25570').then(reachable => {
       isAan[4] = true;
       //=> true
     });
-	isReachable('play.martycraft.net:25571').then(reachable => {
+    isReachable('play.martycraft.net:25571').then(reachable => {
       isAan[5] = true;
       //=> true
     });
-	for (i = 0; i < 6; i++) {
-	if(isAan[i]){
-      message.channel.sendMessage('server online!');
-    } else {
-      message.channel.sendMessage('server offline!');
+    for (i = 0; i < 5; i++) {
+      if (isAan[i]) {
+        message.channel.sendMessage(Namen[i] + 'is online!');
+      } else {
+        message.channel.sendMessage(Namen[i] + 'is offline!');
+      }
     }
-	}
   } else
     if (command === 'avatar') {
       message.reply(message.author.avatarURL);
